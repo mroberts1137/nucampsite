2ddfee0cfeff457e92db38e376b50b52be85f526
mroberts1137 <mroberts1137@gmail.com>
2024-01-11 20:39:55 -0500

    Redux for all

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -35,58 +35,60 @@ const CommentForm = ({ campsiteId }) => {
           Add Comment
         </ModalHeader>
 
-        <Formik
-          initialValues={{
-            rating: undefined,
-            author: '',
-            commentText: ''
-          }}
-          onSubmit={handleSubmit}
-          validate={validateCommentForm}
-        >
-          <Form>
-            <FormGroup>
-              <Label htmlFor='rating'>Rating</Label>
-              <Field name='rating' as='select' className='form-control'>
-                <option>Select...</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Field>
-              <ErrorMessage name='rating'>
-                {(msg) => <p className='text-danger'>{msg}</p>}
-              </ErrorMessage>
-            </FormGroup>
+        <ModalBody>
+          <Formik
+            initialValues={{
+              rating: undefined,
+              author: '',
+              commentText: ''
+            }}
+            onSubmit={handleSubmit}
+            validate={validateCommentForm}
+          >
+            <Form>
+              <FormGroup>
+                <Label htmlFor='rating'>Rating</Label>
+                <Field name='rating' as='select' className='form-control'>
+                  <option>Select...</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Field>
+                <ErrorMessage name='rating'>
+                  {(msg) => <p className='text-danger'>{msg}</p>}
+                </ErrorMessage>
+              </FormGroup>
 
-            <FormGroup>
-              <Label htmlFor='author'>Your Name</Label>
-              <Field
-                name='author'
-                placeholder='Your Name'
-                className='form-control'
-              />
-              <ErrorMessage name='author'>
-                {(msg) => <p className='text-danger'>{msg}</p>}
-              </ErrorMessage>
-            </FormGroup>
+              <FormGroup>
+                <Label htmlFor='author'>Your Name</Label>
+                <Field
+                  name='author'
+                  placeholder='Your Name'
+                  className='form-control'
+                />
+                <ErrorMessage name='author'>
+                  {(msg) => <p className='text-danger'>{msg}</p>}
+                </ErrorMessage>
+              </FormGroup>
 
-            <FormGroup>
-              <Label htmlFor='commentText'>Comment</Label>
-              <Field
-                name='commentText'
-                as='textarea'
-                rows='12'
-                className='form-control'
-              />
-            </FormGroup>
+              <FormGroup>
+                <Label htmlFor='commentText'>Comment</Label>
+                <Field
+                  name='commentText'
+                  as='textarea'
+                  rows='12'
+                  className='form-control'
+                />
+              </FormGroup>
 
-            <Button type='submit' color='primary'>
-              Submit
-            </Button>
-          </Form>
-        </Formik>
+              <Button type='submit' color='primary'>
+                Submit
+              </Button>
+            </Form>
+          </Formik>
+        </ModalBody>
       </Modal>
     </>
   );
